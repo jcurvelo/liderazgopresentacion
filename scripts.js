6b945888880c09646bd703a97c30880a6bb1f9a9
J. Curvelo <38311729+jcurvelo@users.noreply.github.com>
2020-03-24 01:58:01 -0400

    Terminado

--- scripts.js
+++ scripts.js
@@ -1,81 +1,61 @@
 let options = document.getElementsByClassName('options');
 let contentOp = document.getElementsByClassName('contentOp');
-let option = document.getElementsByClassName('option') 
+let option = document.getElementsByClassName('option');
 
-function showContent(content){
-    document.getElementById('content1').style.display = "none";
+for (const x in option) {
+    if (option.hasOwnProperty(x)) {
+        option[x].addEventListener('click', () => {
+            // document.getElementById('container').style.animationName = "startPresentation";
+            document.getElementById('container').style.animationPlayState = "running";
+            setTimeout(() => {
+                document.getElementById('content-container').style.display = "block";
+                document.getElementsByClassName('container')[0].style.transform = "rotate(45deg) scale(0.75)"
+                // document.getElementsByClassName('container')[0].style.transform = "27vw";
+                // document.getElementsByClassName('container')[0].style.height = "27vw";
+            }, 500);
+        });
+    }
+}
+
+for (const x in options) {
+    if (options.hasOwnProperty(x)) {
+        options[x].addEventListener('click', () => {
+            for (const y in option) {
+                if (options.hasOwnProperty(y)) {
+                    option[y].style.backgroundColor = "#9b9b9b49";
+                }
+            }
+            option[x].style.backgroundColor = "#920d4fcb"
+
+            switch (options[x].id) {
+                case 'op1':
+                    hideContent();
+                    document.getElementById('cnt1').style.display = "block";
+                    break;
+                case 'op2':
+                    hideContent();
+                    document.getElementById('cnt2').style.display = "block";
+                    break;
+                case 'op3':
+                    hideContent();
+                    document.getElementById('cnt3').style.display = "block";
+                    break;
+                case 'op4':
+                    hideContent();
+                    document.getElementById('cnt4').style.display = "block";
+                    break;
+    
+                default:
+                    break;
+            }
+        });
+    }
+}
+
+function hideContent(){
     for (const key in contentOp) {
         if (contentOp.hasOwnProperty(key)) {
             contentOp[key].style.display = "none";
         }
     }
-    setTimeout(()=>{
-        document.getElementById('content1').style.display = "flex";
-        document.getElementById(content).style.display = "block";
-    },500);
-}
-
-function animSelected(selected){
-    for (const key in option) {
-        if (option.hasOwnProperty(key)) {
-            option[key].style.transform = "scale(0.8)";
-        }
-    }
-    for (const key in options) {
-        if (options.hasOwnProperty(key)) {
-            options[key].style.backgroundColor = "transparent";
-            options[key].style.border = "none";
-        }
-    }
-    document.getElementsByClassName(selected)[0].style.transform = "scale(1.4)";
-    document.getElementById(selected).style.backgroundColor = "lightgreen";
-    document.getElementById(selected).style.border = "3px solid white";
-}
-
-for (let x = 0; x < options.length; x++) {
-    options[x].addEventListener('click', () => {
-        switch (options[x].id) {
-            case 'op1':
-                document.getElementById('container').style.transform = "rotate(135deg)";
-                animSelected('op1');
-                for (const key in options) {
-                    if (options.hasOwnProperty(key)) {
-                        options[key].style.transform = "rotate(-135deg)";
-                        
-                        showContent('cnt1');
-                    }
-                }
-                break;
-            case 'op2':
-                document.getElementById('container').style.transform = "rotate(45deg)";
-                animSelected('op2');
-                for (const key in options) {
-                    if (options.hasOwnProperty(key)) {
-                        options[key].style.transform = "rotate(-45deg)";
-                        showContent('cnt2');
-                    }
-                }
-                break;
-            case 'op3':
-                document.getElementById('container').style.transform = "rotate(225deg)";
-                animSelected('op3');
-                for (const key in options) {
-                    if (options.hasOwnProperty(key)) {
-                        options[key].style.transform = "rotate(-225deg)";
-                        showContent('cnt3');
-                    }
-                }
-                break;
-            case 'op4':
-                document.getElementById('container').style.transform = "rotate(315deg)";
-                animSelected('op4');
-                for (const key in options) {
-                    if (options.hasOwnProperty(key)) {
-                        options[key].style.transform = "rotate(-315deg)";
-                        showContent('cnt4');
-                    }
-                }
-                break;
-        }
-    })
 }
